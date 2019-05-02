68afe5aff38e169a2db982b10dc01a0f9d5faa8b
a4a881d4 <a4a881d4@163.com>
2019-05-02 16:50:21 +0800

    ring bus

--- src/main/scala/open5g/lib/ringbus/ringbus.scala
+++ src/main/scala/open5g/lib/ringbus/ringbus.scala
@@ -9,14 +9,39 @@ case class constant(value:Int,size:Int) {
   def is(b:Bits) = b === toBits
 }
 
+case class RingBusIO(cfg : RingBusConfig) extends Bundle with IMasterSlave {
+  val D    = Bits(cfg.width bits)
+  val f    = Bool
+  def asMaster() {
+    out(D,f)
+  }
+}
+case class RingTxIO(cfg : RingBusConfig) extends Bundle with IMasterSlave {
+  val d    = Bits(cfg.width bits)
+  val req  = Bool
+  val sop  = Bool
+  def asMaster() {
+    out(d,req)
+    in(sop)
+  }
+}
+case class RingRxIO(cfg : RingBusConfig) extends Bundle with IMasterSlave {
+  val d    = Bits(cfg.width bits)
+  val sop  = Bool
+  def asMaster() {
+    out(d,sop)
+  }
+}
 trait RingBusEP {
   val cfg : RingBusConfig
   val busio = new Bundle {
-    val D    = in Bits(cfg.width bits)
-    val Q    = out Bits(cfg.width bits)
-    val fin  = in Bool
-    val fout = out Bool
-  } 
+    val d = slave(RingBusIO(cfg))
+    val q = master(RingBusIO(cfg))
+  }
+  val hD = RingBusHeader(cfg)
+  val hQ = RingBusHeader(cfg)
+  hD.assignFromBits(busio.d.D(cfg.headerLen-1 downto 0))
+  val Q = Reg(RingBusIO(cfg))
 }
 
 case class RingBusConfig( width : Int
@@ -39,23 +64,10 @@ case class RingBusConfig( width : Int
   val command_read      = constant(2,command_length)
   val command_complete  = constant(3,command_length)
 
-  // len ## busid ## addr ## command
-
   val tag_start         = 16
   val addr_start        = 64
   def headerLen         = command_length + addr_length + busid_length + len_length
-  /*
-  constant state_IDLE   : natural := 0;
-  constant state_PENDING  : natural := 1;
-  constant state_LOADING  : natural := 2;
-  constant state_READY  : natural := 3;
-  constant state_ADDR   : natural := 4;
-  constant state_DATA   : natural := 5;
-  constant state_trans  : natural := 6;
-  constant state_RECV   : natural := 7;
-  constant state_SENDING  : natural := 8;
-  constant state_END    : natural := 9;
-  */
+
 }
 
 case class RingBusHeader(cfg:RingBusConfig) extends Bundle {
@@ -65,39 +77,72 @@ case class RingBusHeader(cfg:RingBusConfig) extends Bundle {
   val len     = Bits(cfg.len_length bits)
 }
 
-case class RingBusControler(cfg:RingBusConfig,Num:Int) extends Component with RingBusEP {
-  val D = RingBusHeader(cfg)
-  val Q = RingBusHeader(cfg)
-  D.assignFromBits(busio.D(cfg.headerLen-1 downto 0))
-  when( busio.fin && 
-    !cfg.command_idle.is(D.command) && 
-    (D.addr.asUInt > U(Num) || D.busid.asUInt =/= U(0)) ) {
-    Q.assignFromBits(B(0,cfg.headerLen bits))
+case class RingBusController(cfg:RingBusConfig,Num:Int) extends Component with RingBusEP {
+  when( busio.d.f && 
+    !cfg.command_idle.is(hD.command) && 
+    (hD.addr.asUInt > U(Num) || hD.busid.asUInt =/= U(0)) ) {
+    hQ.assignFromBits(B(0,cfg.headerLen bits))
   } otherwise {
-    Q := D
+    hQ := hD
   }
-  val outD = Reg(Bits(cfg.width bits)) init 0
   val io = new Bundle {
     val sync = in Bool
   }
   val counter = Reg(UInt(log2Up(cfg.slot) bits))
-  val fout = RegInit(False)
   val delayF = ShiftReg(1,cfg.slot-Num)
   val delayD = ShiftReg(cfg.width,cfg.slot-Num) 
   when(io.sync || counter === U(cfg.slot-1)) {
-    fout := True
+    Q.f := True
     counter := U(0)
-    outD(cfg.headerLen-1 downto 0) := Q.asBits
-    outD(cfg.width-1 downto cfg.headerLen) := busio.D(cfg.width-1 downto cfg.headerLen) 
+    Q.D(cfg.headerLen-1 downto 0) := hQ.asBits
+    Q.D(cfg.width-1 downto cfg.headerLen) := busio.d.D(cfg.width-1 downto cfg.headerLen) 
   } otherwise {
-    fout := False
+    Q.f := False
     counter := counter + 1
-    outD := busio.D
+    Q.D := busio.d.D
   }
   delayF.io.ce    := True
   delayD.io.ce    := True
-  delayF.io.d(0)  := fout
-  delayD.io.d     := outD
-  busio.fout      := delayF.io.q(0)
-  busio.Q         := delayD.io.q
+  delayF.io.d(0)  := Q.f
+  delayD.io.d     := Q.D
+  busio.q.f       := delayF.io.q(0)
+  busio.q.D       := delayD.io.q
+}
+
+case class RingBusNode(cfg:RingBusConfig,Pos:Int) extends Component with RingBusEP {
+  val tx = slave(RingTxIO(cfg))
+  val rx = master(RingRxIO(cfg))
+  val hold = RegInit(False)
+  val rx_sop =  busio.d.f && 
+      hD.busid === B(0) && 
+      hD.addr  === B(Pos) && 
+      !cfg.command_idle.is(hD.command)
+  val tx_sop =  busio.d.f &&
+      tx.req &&
+      (cfg.command_idle.is(hD.command) || rx_sop)
+  when(busio.d.f) {
+    when(tx_sop) {
+      Q.D := tx.d
+    }.elsewhen(rx_sop) {
+      Q.D(cfg.width-1 downto cfg.headerLen) := busio.d.D(cfg.width-1 downto cfg.headerLen)
+      Q.D(cfg.headerLen-1 downto 0) := B(0)
+    }.otherwise {
+      Q.D := busio.d.D
+    }
+    hold := tx_sop
+  }.elsewhen(hold) {
+    Q.D := tx.d
+  }
+  Q.f := busio.d.f
+  rx.d := busio.d.D
+  busio.q := Q
+  tx.sop := tx_sop
+  rx.sop := rx_sop
+}
+
+case class RingBus(cfg:RingBusConfig,Num:Int) extends Component {
+  val controller = RingBusController(cfg,Num)
+  val tx = Vec(slave(RingTxIO(cfg)),Num)
+  val rx = Vec(master(RingRxIO(cfg)),Num)
+  val node = List.fill(Num)(RingBusNode(cfg))
 }
