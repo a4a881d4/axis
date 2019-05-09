17372fee4339278c4b4a8f9322d6b17f0097e864
a4a881d4 <a4a881d4@163.com>
2019-05-09 20:26:46 +0800

    tsp

--- src/main/scala/open5g/lib/tsp/tsp.scala
+++ src/main/scala/open5g/lib/tsp/tsp.scala
@@ -3,6 +3,7 @@ package open5g.lib.tsp
 import spinal.core._
 import spinal.lib._
 
+import open5g.lib.common.Constant
 /**
  *  Timing Sequence Processor(TSP)
  *
@@ -18,17 +19,18 @@ import spinal.lib._
  *  format of instruction
  *    auto Bool
  *    mask 3 bits
- *    op   Bits(2 bits)
- *      nop B(0)
- *      alu B(1)
+ *    exec Bool
+ *      
+ *      alu B(0)
  *        loadr
  *        loadf
  *        
- *      exec B(2)
+ *      exec B(1)
  *        gpio
  *        DMAD
  *        DMAR
  *        trigger
+ *
  *      pcc B(3)
  *        jump
  *        calln
@@ -48,11 +50,11 @@ import spinal.lib._
  *      DMAD    B(2,4 bits) ## (DMA_ch 4 bits, from 12, to 12, len 9 )
  *      CHDR    B(3,4 bits) ## (DMA_ch 4 bits, dest 16 bits, src 16 bits, type 4 bits, addr 17 )
  *      
- *      jumps   B(0,4bits)  ## (stage SGWidth bits, pad DWidth-4-SGWidth-IAWidth bits, addr IAWidth bits)
- *      jumpf   B(1,4bits)  ## (flag 16 bits, pad DWidth-4-16-IAWidth bits, addr IAWidth bits)
- *      jumpr   B(2,4bits)  ## (flag 16 bits, pad DWidth-4-16-IAWidth bits, addr IAWidth bits)
- *      callf   B(3,4bits)  ## (flag 16 bits, pad DWidth-4-16-IAWidth bits, addr IAWidth bits)
- *      calla   B(4,4bits)  ## (arg 16 bits, addr IAWidth bits)
+ *      jumps   B(1,4bits)  ## (stage SGWidth bits, pad DWidth-4-SGWidth-IAWidth bits, addr IAWidth bits)
+ *      jumpf   B(2,4bits)  ## (flag 16 bits, pad DWidth-4-16-IAWidth bits, addr IAWidth bits)
+ *      jumpr   B(3,4bits)  ## (flag 16 bits, pad DWidth-4-16-IAWidth bits, addr IAWidth bits)
+ *      callf   B(4,4bits)  ## (flag 16 bits, pad DWidth-4-16-IAWidth bits, addr IAWidth bits)
+ *      calla   B(5,4bits)  ## (arg 16 bits, addr IAWidth bits)
  *      intr    B(8,4bits)  ## (thread TWidth bits, pad DWidth-4-TWidth-IAWidth bits, addr IAWidth bits)
  *      ret     B(9,4bits)  ## (pad DWidth-4 bits)
  *      reti    B(10,4bits) ## (pad DWidth-4 bits)
@@ -60,4 +62,56 @@ import spinal.lib._
  *
  */
 
- case class TSPConfig()
+case class TSPConfig( NThread : Int,
+                      MStage  : Int,
+                      IWidth  : Int,
+                      RWidth  : Int,
+                      IAWidth : Int,
+                      SWidth  : Int
+                      ) {
+  def TWidth  = log2Up(NThread)
+  def SGWidth = log2Up(MStage)
+  def FWidth  = RWidth - IAWidth - (3*SWidth)
+  def PWidth  = IWidth - 10
+}
+
+object TSPConfig {
+  def default = TSPConfig(32,16,72,72,9,16,9)
+}
+
+class FlagIO(cfg:TSPConfig) extends Bundle with IMasterSlave {
+  val carry : Bool
+  val zero  : Bool
+  val state : Bits(3 bits)
+  val gpio  : Bits(3 bits)
+  val other : Bits(cfg.FWidth-8 bits)
+  def asMaster = {
+    out(carray,zero,state,gpio,other)
+  }
+}
+
+class RegIO(cfg:TSPConfig) extends Bundle with IMasterSlave {
+  val pc = UInt(cfg.IAWidth bits)
+  val state = Vec(UInt(cfg.SWidth bits),3)
+  val flag  = new FlagIO(cfg)
+  def asMater = {
+    out(pc,stage,flag)
+  }
+}
+
+class Instruction(cfg:TSPConfig) extends Bundle with IMasterSlave {
+  val auto   = Bool
+  val mask   = Bits(3 bits)
+  val exec   = Bool
+  val pluged = Bits(cfg.PWidth bits)
+  def asMaster = {
+    out(auto,mask,exex,pluged)
+  }
+}
+
+object Instruction {
+  val nop  = Constant(0,2)
+  val alu  = Constant(1,2)
+  val exec = Constant(2,2)
+  val pcc  = Constant(3,2)
+}
